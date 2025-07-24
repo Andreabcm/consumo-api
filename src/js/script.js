c0c0465272612bb252ca8d60447e19d4cddf8cb8
Andrea <andreabcm16@gmail.com>
2025-07-24 16:00:29 +0200

    practice in superKode

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,66 @@
-// const animes =[
-//   {
-//     id:1,
-//     title: 'Magic Kaito',
-//     year:1987
-//   },
-//   {
-//     id:2,
-//     title: 'Yaiba',
-//     year:1988
-//   },
-//   {
-//     id:3,
-//     title:'Detective Conan',
-//     year:1994
-//   },
-// ]
-
-// function getAnime(){
-//     setTimeout(()=>{
-//         console.log(animes)
-//     },2000)
-// }
-
-// getAnime()
-
-const getData=async()=>{
-    let characters = ''
-    const response = await fetch("https://rickandmortyapi.com/api/character")
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
+const getData = async () => {
+    let characters = "";
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
     data.results.map(character => {
-        console.log(character)
-        characters = characters+ `
+        characters = characters + `
             <li>
                 <img src="${character.image}" alt="${character.name}">
                 <h2>${character.name}</h2>
             </li>
         `
-    });
-    document.getElementById('character-list').innerHTML = characters
-
+    })
+    
+    document.getElementById("character-list").innerHTML = characters;
 }
 
 getData()
+
+
+/* Este código sirve para obtener los datos de una API (como la de Rick and Morty) y 
+mostrarlos por consola. */
+
+/*
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error; // Re-throw the error for further handling if needed
+  }
+}
+
+
+getData('https://rickandmortyapi.com/api/character')
+  .then(data => {
+    console.log('Data fetched successfully:', data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+const getStatus = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.status;
+  } catch (error) {
+    console.error('Error fetching status:', error);
+    throw error; // Re-throw the error for further handling if needed
+  }
+}
+getStatus('https://rickandmortyapi.com/api/character')
+  .then(status => {
+    console.log('Status fetched successfully:', status);
+  })
+  .catch(error => {
+    console.error('Error fetching status:', error);
+  });
+*/
